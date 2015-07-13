8e30863dee79cdbf30dcc7d88d299d822a8fa0f4
JackHou <vjack070707@gmail.com>
2015-07-13 23:21:36 +0800

    gitignore

---
+++ youbiker.rb
@@ -0,0 +1,91 @@
+require "csv"
+require "json"
+require "open-uri"
+require "net/http"
+require "slop"
+require "timers"
+
+class Crawl
+
+    def initialize(options)
+        @options = options
+        @file_path = @options[:destination] ? "#{@options[:destination]}/youbike-#{Time.now.to_i}" :  "youbike-#{Time.now.to_i}"
+
+        get_file
+    end
+
+    def loop_file
+        @now = []
+        @station = []
+        @youbike_json.each do |data|
+            if @options[:now]
+                @now << data.select { |key, value| ["iid", "sno", "sna", "sbi"].include?(key) }
+            end
+            if @options[:station]
+                @station << data.select { |key, value| ["iid", "sna", "sno", "tot", "sarea", "lat", "lng", "ar", "saraen", "snaen", "aren", "nbcnt", "bemp", "act"].include?(key)  }
+            end
+        end
+
+        if @options[:now]
+            self.to_json
+        end
+
+        if @options[:station]
+            self.to_station_info
+        end
+    end
+
+
+    def to_station_info
+        puts "Saving station information - #{@file_path}_station"
+
+        File.open("#{@file_path}_station.json", "wb") do |file|
+            file << @station.to_json
+        end
+    end
+
+
+    def to_json
+        puts "Saving exist situation - #{@file_path}"
+
+        File.open("#{@file_path}.json", "wb") do |file|
+            file << @now.to_json
+        end
+    end 
+
+
+    private
+
+    def get_file
+        youbike_data = Net::HTTP.get(URI.parse("http://opendata.dot.taipei.gov.tw/opendata/gwjs_cityhall.json"))
+        @youbike_json = JSON.parse(youbike_data)['retVal']
+    end
+
+end
+
+begin
+    opts = Slop.parse do |o|
+        o.bool '-w', '--watch', '[boolean] enable crawl every 10 minutes', default: false
+        o.bool '-n', '--now', '[boolean] get existing now', default: true
+        o.bool '-s', '--station', '[boolean] get station info', default: false
+        o.string '-d', '--destination', '[string] data destination; ex: ruby main.rb -d data; then save data in `data` folder'
+        o.on '--help' do
+          puts o
+          exit
+        end
+    end
+    opts = opts.to_hash
+    crawl = Crawl.new(opts)
+    crawl.loop_file
+    if opts[:watch]
+        timers = Timers::Group.new
+        five_second_timer = timers.every(5) {
+            p 'Get new'
+            crawl = Crawl.new(opts)
+            crawl.loop_file
+            p '==================='
+        }
+        loop { timers.wait }
+    end
+end
+
